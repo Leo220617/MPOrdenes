f6720ffb0ebcba2d9b5440f1fe7d6d48f13b189a
Leo220617 <32038650+Leo220617@users.noreply.github.com>
2022-07-19 21:57:43 -0600

    hbbkjb

---
+++ Sicsoft.Checkin.Web/Servicios/ICrudApi.cs
@@ -0,0 +1,50 @@
+﻿using Refit;
+using Sicsoft.Checkin.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Sicsoft.Checkin.Web.Servicios
+{
+     
+
+    public interface ICrudApi<TEntity, TKey> where TEntity : class
+    {
+       
+        [Post("")]
+        Task<TEntity[]> AgregarBulk([Body] TEntity[] payload);
+
+        [Post("")]
+        Task<TEntity> Agregar([Body] TEntity payload);
+        [Post("")]
+        Task<TEntity> CambiarClave([Body] TEntity payload);
+
+        [Get("")]
+        Task<TEntity> Login(string email, string clave);
+
+        [Get("")]
+        Task<TEntity[]> ObtenerLista<TQuery>(TQuery q);
+
+        [Get("/Compañias")]
+        Task<TEntity[]> ObtenerCompañias(string email);
+
+        [Get("")]
+        Task<TEntity> ObtenerListaEspecial<TQuery>(TQuery q);
+
+        [Get("/Consultar")]
+        Task<TEntity> ObtenerPorId(int id);
+
+        [Post("/Actualizar")]
+        Task Editar( [Body]TEntity payload);
+ 
+        [Delete("/Eliminar")]
+        Task Eliminar(int id);
+
+        [Post("/Eliminar")]
+        Task EliminarUsuario(int id, string CedulaJuridica);
+
+      
+
+ 
+    }
+}
